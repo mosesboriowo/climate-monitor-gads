a33c40f76dab23a2abdddb5ef3b6637a593fe763
mordern_geek <dyopal@gmail.com>
2022-11-22 14:20:49 +0100

    Progress Report
    Updated

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ Tasks
 
 1 - Find and Fetch reliable Climate air pollution data from an API  - done 
 
-2 - Build simple app to measure air pollution based on geolocation by: #In progress
- - fetch latitude and longitude location data based on city that user inputs 
- - return air pollution index data 
- - create a simple html/ frontend to interact with data 
+2 - Build simple app to measure air pollution based on geolocation by: 
+ - fetch latitude and longitude location data based on city that user inputs - done
+ - return air pollution index data and recommendation - done
+ - create a simple html/ frontend to interact with data - in progress
  
-3 - test for functionality 
+3 - test for functionality - in progress
 
-4 - Package as flask app and make available on docker as image that anyone can pull and run 
+4 - Package as flask app and make available on docker as image that anyone can pull and run - #in pogress
--- app.py
+++ app.py
@@ -1,17 +1,85 @@
-#GADS Climate CHange Individual Project by Damilola Olaide Banjoko
+#GADS Individual Project by Damilola Olaide Banjoko
 
-#Climate Change Monitor to measure air pollution by location 
+#Climate Change Monitor to measure effect of air pollution data on climate change and increase awareness of environmental pollution globally
 
-from flask import Flask, jsonify, request
+#This project uses a CNN API to measure the current air pollution data based on the user's specified location and informs or recommmends appropriate action based on the retrieved Air Quality Index 
+
+
+#env for the location and climate // pip install geopy && pip install meteostat 
+#Install Python 3.6 or higher 
+
+from flask import Flask, jsonify, request, session, render_template, url_for, redirect, flash
+import logging
+import json
+import http.client
+from logging import Logger
+from datetime import datetime
+
+from geopy.geocoders import Nominatim #importing a py geolocation library
+
+geofinder = Nominatim(user_agent="app") # initializing the library
+
 
 app = Flask(__name__) 
 
-@app.route('/', methods =['GET', 'POST'])
+@app.route('/', methods=('GET', 'POST'))
 def main():
-    if request.method == 'POST' and 'username' in request.form and 'city' in request.form and 'country' in request.form:
-        username = request.form['username']
-        city = request.form['city']
-        country = request.form['country']
+	if request.method == 'POST'  and 'state' in request.form:
+		name = request.form['name']
+		Location = requestform['state']
+		session['state'] = request.form['state']
+		return redirect(url_for('result'))
+	else:
+		flash('One or more required fields is missing')
+    return render_template('index.html')
+	
+	
+@app.route('/air_index.html')
+def result():
+	if 'state' in session:
+		state = session['state']
+		location = geofinder.geocode(state)
+	
+		longitude = location.longitude
+		latitude = location.latitude
+		
+			
+		conn = http.client.HTTPSConnection("rapidweather.p.rapidapi.com")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+		headers = {
+			'X-RapidAPI-Key': "f643f650d72mshc320da1d0f67a3bp1dad5bjsn7f8bf809aafa",
+			'X-RapidAPI-Host': "rapidweather.p.rapidapi.com"
+			}
+
+		conn.request("GET", "/data/2.5/air_pollution?lat="+ latitude + "&lon=" + longi + "", headers=headers)
+
+		res = conn.getresponse()
+		data = res.read()
+
+
+		all = data.decode("utf-8")
+		pollution_index = json.loads(all)
+		print(pollution_index)
+		#print(all)
+		print(pollution_index['list'][0]['main']['aqi']) 
+		
+		result = pollution_index['list'][0]['main']['aqi']
+		
+		if result <= 3:
+			print('Ideal: The Air Pollution Index is low at your location')
+		elif 3 < result <= 6:
+			print('Warning:The Air Pollution is Moderate at your loction')
+		elif 6 < result <=10:
+			print('At Risk: The Air Pollution Index is very high, take precaution')
+		else:
+			print('Emergency: The Air Pollution Index is extreme today. Report status to appropriate local authorities ')
+	
+		session.pop('state', None)
+		return render_template('air_index.html')
+	else:
+		return render_template('index.html')
+	
+
+
+if __name__ == "__main__": 
+	app.run(debug=True)
